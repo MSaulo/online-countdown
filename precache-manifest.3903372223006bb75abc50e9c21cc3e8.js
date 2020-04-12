12c3bdd11414cf90757e8865b8c2b808ce254337
msaulo <saulobr@live.com>
2020-04-12 20:20:56 -0300

    Updates

---
+++ precache-manifest.3903372223006bb75abc50e9c21cc3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89784089c834124d657d9593d8ab157e",
+    "url": "/online-countdown/index.html"
+  },
+  {
+    "revision": "7676122f3a4f5c62b843",
+    "url": "/online-countdown/static/css/main.bec4fedb.chunk.css"
+  },
+  {
+    "revision": "bb0171863b37d4cfa34c",
+    "url": "/online-countdown/static/js/2.9bbda9da.chunk.js"
+  },
+  {
+    "revision": "4fbaa35f2f79d228cfc32cc41071d7fe",
+    "url": "/online-countdown/static/js/2.9bbda9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7676122f3a4f5c62b843",
+    "url": "/online-countdown/static/js/main.297bd126.chunk.js"
+  },
+  {
+    "revision": "31ce78ddf33e60910dcb",
+    "url": "/online-countdown/static/js/runtime-main.07c95c0b.js"
+  }
+]);
